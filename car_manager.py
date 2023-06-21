61c2145af0e51fcf87c1d4eb1e93914a0910719d
Saami Abbas Khan <102609667+Kingsam70@users.noreply.github.com>
2023-06-21 22:01:07 +0530

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -28,22 +28,3 @@ class CarManager:
         """increment in speed by +5 """
         self.move_distance += 5
 
-
-
-
-
-# Please ignore the issue where the cars are so near or overlaping with each other i could have just checked for
-# overlaping and repositioned that very car to different position but it could have overlaped to another car in
-# list_of_cars, In order to prevent collisions with all the cars the spawning of cars should be at a fixed rate or
-# the spawning should be at a fixed place meaning spawining should only take place at pre defined positions
-# if using 10 pace distance if you'd use 30 it'll be fixed but the speed of the cars will hugely impace(increase)
-# , In our example neither of the two are taken under consideration
-
-
-# also pls bear that if you collide with a car in x-direction/horizontally the car will overlap you meaning you'll get
-# into the car, meaning the programm will not end on touching cause due to different height and width of the gif car
-# images also turtle size is fixed i.e 20*20
-
-# Also you can add high score mechanism using txt file but not felt the need
-
-# Turtle cannot move backwards
